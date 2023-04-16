dbfb9a85813132e590b2966a00bb9d827f28e63c
pabcrudel <pabcrudel@gmail.com>
2023-04-17 00:20:03 +0200

    Use blogStore

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { blogStore } from './stores/blog'
+
+blogStore().fetchArticles()
 </script>
 
 <template>
