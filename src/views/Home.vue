b6d349ca4f07e7898a49f8840b992b8998577737
pabcrudel <pabcrudel@gmail.com>
2023-04-17 23:19:04 +0200

    Added some examples

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,15 @@
   <div class="home">
     <h1>This is the home page</h1>
     <input type="search" name="search" id="search" v-model="search">
-    <p v-for="post in blogStore.searchPosts(search)" :key="post.name" v-html="post.name"/>
+    <p v-for="post in blogStore.searchPosts(search)" :key="post.name" v-html="post.navigation.title"/>
     <hr>
     <p v-for="(category, index) in blogStore.categories" :key="index" v-html="category"/>
     <hr>
-    <p v-for="post in blogStore.getRelatedPostsByCategory('moBile')" :key="post.name" v-html="post.name"/>
+    <p v-for="post in blogStore.getRelatedPostsByCategory('moBile')" :key="post.name" v-html="post.navigation.title"/>
+    <hr>
+    <p v-for="post in blogStore.getUnrelatedPostsByCategory('moBile')" :key="post.name" v-html="post.navigation.title"/>
+    <hr>
+    <p v-html="blogStore.getPostByName('docker-swarm').navigation.title"/>
     <hr>
   </div>
 </template>
@@ -19,3 +23,7 @@ const blogStore = useBlogStore();
 
 let search = ref("");
 </script>
+
+<style scoped>
+hr {margin: 1rem 0;}
+</style>
