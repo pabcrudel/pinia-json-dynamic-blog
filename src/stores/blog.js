85b5fd8d11c181e4240f53c04eb056313f07a3a1
pabcrudel <pabcrudel@gmail.com>
2023-04-17 23:03:28 +0200

    Moved 'searchPosts' to getters and created one filter

--- src/stores/blog.js
+++ src/stores/blog.js
@@ -5,6 +5,7 @@ export const useBlogStore = defineStore({
     id: 'blog',
     state: () => ({
         posts: [],
+        categories: new Set(),
         loading: true,
         error: null,
     }),
@@ -13,6 +14,8 @@ export const useBlogStore = defineStore({
             try {
                 const response = await axios.get('/main.json');
                 this.posts = response.data;
+
+                this.posts.map(post => post.categories.map(category => this.categories.add(category)));
             }
             catch (error) {
                 console.log(error)
@@ -21,23 +24,37 @@ export const useBlogStore = defineStore({
 
             this.loading = false;
         },
+    },
+    getters: {
         /**
          * The function searches through an array of posts and returns any posts that contain all or some of the search terms. 
          * @param searchTerm - The search term that the user is looking for in the posts. It is converted to lowercase and split into an array of individual words for easier comparison with the content of the posts.
          * @returns The function `searchPosts` is returning an array of posts that match the search term provided as an argument. The posts are filtered based on whether their content includes every or some of the search terms provided.
          */
-        searchPosts(searchTerm) {
-            searchTerm = searchTerm.toLowerCase().split(" ");
+        searchPosts: (state) => {
+            return (searchTerm) => {
+                const terms = searchTerm.toLowerCase().split(" ");
 
-            const matchedPosts = this.posts.filter(post => {
-                const content = post.content.toLowerCase();
-                return searchTerm.every(term => content.includes(term)) || searchTerm.some(term => content.includes(term));
-            });
+                function getPostBySearchType(searchType) {
+                    return state.posts.filter(post => {
+                        const allPostValues = Object.values(post).join(" ").toLowerCase();
+                        return terms[searchType](term => allPostValues.match(term));
+                    });
+                }
 
-            return matchedPosts;
-        }
-    },
-    getters: {
+                let matchedPosts = getPostBySearchType("every");
 
+                if (matchedPosts.length === 0) {
+                    matchedPosts = getPostBySearchType("some");
+                }
+
+                return matchedPosts;
+            };
+        },
+        getRelatedPostsByCategory: (state) => {
+            return (_category) => {
+                return state.posts.filter(post => post.categories.find(category => category.toLowerCase() === _category.toLowerCase()));
+            };
+        },
     },
 });
