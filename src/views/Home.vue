b23862c97ba1a87f55bfa3b9bc885223ca1d1e17
pabcrudel <pabcrudel@gmail.com>
2023-04-17 23:01:42 +0200

    Tested some blog functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="home">
     <h1>This is the home page</h1>
+    <input type="search" name="search" id="search" v-model="search">
+    <p v-for="post in blogStore.searchPosts(search)" :key="post.name" v-html="post.name"/>
+    <hr>
+    <p v-for="(category, index) in blogStore.categories" :key="index" v-html="category"/>
+    <hr>
+    <p v-for="post in blogStore.getRelatedPostsByCategory('moBile')" :key="post.name" v-html="post.name"/>
+    <hr>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import { useBlogStore } from '../stores/blog'
+
+const blogStore = useBlogStore();
+
+let search = ref("");
+</script>
