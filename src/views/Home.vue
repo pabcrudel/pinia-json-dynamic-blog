61ad4833bad93d30a5377959201f8643907da534
pabcrudel <pabcrudel@gmail.com>
2023-04-20 19:50:40 +0200

    Remove search bar from home and move it to blog

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,18 @@
 <template>
   <div class="home">
-    <h1>This is the home page</h1>
+    <h1>Bienvenido a nuestra Página de Programación</h1>
 
     <hr>
-    <h2>Search bar</h2>
-    <input type="search" name="search" id="search" v-model="search">
-    <LinkedPostList :posts="blogStore.searchPosts(search)"/>
-    <hr>
-
     <h2>Categories</h2>
     <p v-for="(category, index) in blogStore.categories" :key="index" v-html="category" />
-    <hr>
-
-    <h2>Related post</h2>
-    <p v-for="post in blogStore.getPostsByCategory('moBile')" :key="post.name" v-html="post.navigation.title" />
-    <hr>
-
-    <h2>Unrelated posts</h2>
-    <p v-for="post in blogStore.getPostsByCategory('moBile', false)" :key="post.name" v-html="post.navigation.title" />
     <hr>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import { useBlogStore } from '../stores/blog';
 
-import LinkedPostList from '../components/LinkedPostList .vue';
-
 const blogStore = useBlogStore();
-
-let search = ref("");
 </script>
 
 <style scoped>
