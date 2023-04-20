80735c80fb7fb73abcef9e3b236927b5b958d892
pabcrudel <pabcrudel@gmail.com>
2023-04-20 19:14:13 +0200

    Add LinkedPostList component

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -3,11 +3,16 @@
     <hr>
     <p v-html="post.content" />
     <hr>
+    <LinkedPostList :title="'Articulos Relacionados'" :posts="blogStore.getPostsByCategories(post.categories)"/>
+    <hr>
+    <LinkedPostList :title="'Otros articulos que podrian interesarte'" :posts="blogStore.getPostsByCategories(post.categories, false)"/>
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router';
 import { useBlogStore } from '../stores/blog';
+
+import LinkedPostList from '../components/LinkedPostList .vue';
 
 const route = useRoute();
 
