7eb73e56feb86e200c52edaabf24e62bc6e7230d
pabcrudel <pabcrudel@gmail.com>
2023-04-20 12:08:46 +0200

    Created post view

---
+++ src/views/Post.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1 v-html="post.navigation.title"/>
+    <hr>
+    <p v-html="post.content"/>
+    <hr>
+</template>
+
+<script setup>
+import {useRoute} from 'vue-router';
+import { useBlogStore } from '../stores/blog';
+
+const route = useRoute();
+
+const blogStore = useBlogStore();
+const post = blogStore.getPostBySlug(route.params.slug);
+</script>
