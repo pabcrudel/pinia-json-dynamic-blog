b952f52a6edbbbc83f4b3e5f757637daf553742d
pabcrudel <pabcrudel@gmail.com>
2023-04-20 12:08:13 +0200

    Search bar results now are links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="home">
     <h1>This is the home page</h1>
-    <input type="search" name="search" id="search" v-model="search">
-    <p v-for="post in blogStore.searchPosts(search)" :key="post.name" v-html="post.navigation.title"/>
+
     <hr>
+    <h2>Search bar</h2>
+    <input type="search" name="search" id="search" v-model="search">
+    <div class="results">
+      <RouterLink v-for="post in blogStore.searchPosts(search)" :key="post.name" v-html="post.navigation.title" :to="'/' + post.metadata.slug"/>
+    </div>
+    <hr>
+
+    <h2>Categories</h2>
     <p v-for="(category, index) in blogStore.categories" :key="index" v-html="category"/>
     <hr>
+
+    <h2>Related post</h2>
     <p v-for="post in blogStore.getRelatedPostsByCategory('moBile')" :key="post.name" v-html="post.navigation.title"/>
     <hr>
+
+    <h2>Unrelated posts</h2>
     <p v-for="post in blogStore.getUnrelatedPostsByCategory('moBile')" :key="post.name" v-html="post.navigation.title"/>
-    <hr>
-    <p v-html="blogStore.getPostByName('docker-swarm') ? blogStore.getPostByName('docker-swarm').navigation.title : ''"/>
     <hr>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import { useBlogStore } from '../stores/blog'
+import { useBlogStore } from '../stores/blog';
 
 const blogStore = useBlogStore();
 
@@ -26,4 +35,12 @@ let search = ref("");
 
 <style scoped>
 hr {margin: 1rem 0;}
+h2 {
+  text-decoration: underline lightblue;
+}
+.results {
+  display: flex;
+  flex-direction: column;
+  gap: .5rem;
+}
 </style>
