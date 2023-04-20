d51f953156a5253b44b4f5d8f8fc47b235df33af
pabcrudel <pabcrudel@gmail.com>
2023-04-20 19:46:35 +0200

    Added Blog view

---
+++ src/views/Blog.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Explora el mundo de la programación</h1>
+
+    <hr>
+    <h2>¿Que estas buscando?</h2>
+    <input type="search" name="search" id="search" v-model="search">
+    <LinkedPostList :posts="blogStore.searchPosts(search)" />
+    <hr>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useBlogStore } from '../stores/blog';
+
+import LinkedPostList from '../components/LinkedPostList .vue';
+
+const blogStore = useBlogStore();
+
+let search = ref("");
+</script>
