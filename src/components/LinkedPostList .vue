0b23a7600a0fb3095c53aa3dc6aef27ccd4a31f4
pabcrudel <pabcrudel@gmail.com>
2023-04-20 19:13:02 +0200

    Add LinkedPostList component

---
+++ src/components/LinkedPostList .vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="linkedPostList">
+        <h3 v-if="title" v-html="title" />
+        <RouterLink class="listedPosts" v-for="post in posts" :key="post.name" v-html="post.navigation.title"
+            :to="'/' + post.metadata.slug" />
+    </section>
+</template>
+
+<script setup>
+defineProps({
+    title: String,
+    posts: {
+        type: Set,
+        required: true
+    }
+});
+</script>
+
+<style scoped>
+.linkedPostList {
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+}
+</style>
