c24d6915c2859f555dacd3b9f110c202d2d5b7fb
pabcrudel <pabcrudel@gmail.com>
2023-04-21 21:31:15 +0200

    Use new component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,24 +3,19 @@
     <h1>Bienvenido a nuestra Página de Programación</h1>
 
     <hr>
-    <h2>Categories</h2>
-    <RouterLink v-for="(category, index) in blogStore.categories" :key="index" v-html="category"
-      :to="{ name: 'blog', query: { searchterm: normalizeString(category) } }" />
+    <LinkedSearchTermList :title="'Categorias'" :terms="blogStore.categories"/>
     <hr>
-    <h2>Etiquetas</h2>
-    <RouterLink v-for="(tag, index) in blogStore.tags" :key="index" v-html="tag"
-      :to="{ name: 'blog', query: { searchterm: normalizeString(tag) } }" />
+    <LinkedSearchTermList :title="'Etiquetas'" :terms="blogStore.tags"/>
     <hr>
   </div>
 </template>
 
 <script setup>
 import { useBlogStore } from '../stores/blog';
-import useStringEditor from '../composables/useStringEditor';
+
+import LinkedSearchTermList from '../components/LinkedSearchTermList.vue';
 
 const blogStore = useBlogStore();
-
-const { normalizeString } = useStringEditor();
 </script>
 
 <style scoped>
