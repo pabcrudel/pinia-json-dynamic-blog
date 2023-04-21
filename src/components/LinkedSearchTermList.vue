1973182378e36d64a906f1693b34f63704e8cbd7
pabcrudel <pabcrudel@gmail.com>
2023-04-21 21:30:07 +0200

    Component that displays linked search terms

---
+++ src/components/LinkedSearchTermList.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="linkedSearchTermList">
+        <h3 v-if="title" v-html="title" />
+        <RouterLink v-for="(term, index) in terms" :key="index" v-html="term"
+            :to="{ name: 'blog', query: { searchterm: normalizeString(term) } }" />
+    </section>
+</template>
+
+<script setup>
+import useStringEditor from '../composables/useStringEditor';
+
+defineProps({
+    title: String,
+    terms: {
+        type: Set,
+        required: true
+    }
+});
+
+const { normalizeString } = useStringEditor();
+</script>
+
+<style scoped>
+.linkedSearchTermList {
+    display: flex;
+    flex-direction: column;
+    gap: .25rem;
+}
+</style>
