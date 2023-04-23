5f032195504f727354bfdaed82ba87a72d74e466
pabcrudel <pabcrudel@gmail.com>
2023-04-23 17:54:16 +0200

    Added config in order to deploy to gh-pages

--- src/stores/blog.js
+++ src/stores/blog.js
@@ -16,7 +16,7 @@ export const useBlogStore = defineStore({
     actions: {
         async fetchPosts() {
             try {
-                const response = await axios.get('/posts.json');
+                const response = await axios.get(process.env.NODE_ENV === "production" ? "/pinia-json-dynamic-blog/" : "/" + "posts.json");
                 response.data.forEach(post => this.posts.add(post));
 
                 this.storeCategoriesAndPaths();
