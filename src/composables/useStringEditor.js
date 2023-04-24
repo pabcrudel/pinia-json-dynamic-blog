3f7d67ecc7a46e7214370d67c93f4bbc77c32528
pabcrudel <pabcrudel@gmail.com>
2023-04-24 11:11:25 +0200

    Added string validation

--- src/composables/useStringEditor.js
+++ src/composables/useStringEditor.js
@@ -1,29 +1,35 @@
 export default function useStringEditor() {
+  const isString = (text) => {
+    if (typeof text !== "string") { throw new Error(`${text} is not valid. Input must be a string.`); }
+    else { return true; };
+  };
 
   function normalizeString(text) {
-    return text
-      // eliminar acentos
-      .normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-      // sustituir espacios por guiones "-"
-      .replace(/ /g, '-')
-      // eliminar puntos
-      .replace(/\./g, "")
-      // poner todo a minusculas
-      .toLowerCase();
-  };
-
-  function capitalizeFirstLetter(text) {
-    return text.charAt(0).toUpperCase() + text.slice(1);
-  };
-
-  function replaceDashWithSpace(text) {
-    return text.replace(/-/g, ' ');
+    if (isString(text)) {
+      return text
+        // eliminar acentos
+        .normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+        // sustituir espacios por guiones "-"
+        .replace(/ /g, '-')
+        // eliminar puntos
+        .replace(/\./g, "")
+        // poner todo a minusculas
+        .toLowerCase();
+    };
   };
 
   function capitalizeFirstLetterAndReplaceDash(text) {
-    text = replaceDashWithSpace(text);
-    text = capitalizeFirstLetter(text);
-    return text;
+    if (isString(text)) {
+      text = replaceDashWithSpace(text);
+      text = capitalizeFirstLetter(text);
+      return text;
+    };
+  };
+  function replaceDashWithSpace(text) {
+    return text.replace(/-/g, ' ');
+  };
+  function capitalizeFirstLetter(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
   };
 
   return {
