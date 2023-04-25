a1204d85320e1acbb46911d090356795aef8c5b9
pabcrudel <pabcrudel@gmail.com>
2023-04-25 21:18:33 +0200

    Created component to move between topics

---
+++ src/components/ChangeBlogTopic.vue
@@ -0,0 +1,12 @@
+<template>
+    <section class="topicSwitch">
+        <button v-for="topic in blogStore.topics" :key="topic.name" v-html="topic.title"
+            @click="blogStore.fetchPosts(topic.name)" />
+    </section>
+</template>
+
+<script setup>
+import { useBlogStore } from '../stores/blog';
+
+const blogStore = useBlogStore();
+</script>
