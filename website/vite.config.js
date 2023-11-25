5b023e644ef2e2e928538f251181f8f8f4298086
pabcrudel <pabcrudel@gmail.com>
2023-11-25 14:17:27 +0100

    change: Wrapped project inside website folder

--- website/vite.config.js
+++ website/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/pinia-json-dynamic-blog/" : "/",
+  base: './', // Configure relative paths on production
   plugins: [vue()],
   resolve: {
     alias: {
